acb29852f128d60a8763a6f40a87e884326f724e
Matthew Ritter <mritter2283@gmail.com>
2018-10-14 11:18:17 -0400

    Added bootstrap, Created rootReducer and app Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import App from "./components/app";
+import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware())
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
